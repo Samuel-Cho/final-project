72a8af0b401fc93c02a19f9c33e9ddb3866336c8
SamuelCho1 <samuel.ms.cho@gmail.com>
2021-04-06 19:07:25 +0000

    Added app name and app drawer icon to header. Added link tags to html for font awesome and font families. #1

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -3,7 +3,12 @@ import React from 'react';
 export default function Header(props) {
   return (
     <header>
-
+      <div className="drawer-container">
+        <i className="fas fa-bars"></i>
+      </div>
+      <div className="app-name-container">
+        <h1 className="app-name">Teeter Taste</h1>
+      </div>
     </header>
   );
 }
