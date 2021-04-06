b5e4dc23810ad0c75faf42ecb0110961a7d8488a
SamuelCho1 <samuel.ms.cho@gmail.com>
2021-04-06 20:03:23 +0000

    Styled header to figma for mobile view. #1

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 export default function Header(props) {
   return (
     <header>
-      <div className="drawer-container">
+      <div className="drawer-container column-one-fourth">
         <i className="fas fa-bars"></i>
       </div>
-      <div className="app-name-container">
+      <div className="app-name-container column-three-fourth">
         <h1 className="app-name">Teeter Taste</h1>
       </div>
     </header>
