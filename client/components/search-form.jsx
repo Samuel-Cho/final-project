960b5bfeda77d06da196ea7624e7ee598f388668
SamuelCho1 <samuel.ms.cho@gmail.com>
2021-04-07 01:06:20 +0000

    Edit SearchForm component to return 2 form elements and hide 2nd form based on media query. #1

--- client/components/search-form.jsx
+++ client/components/search-form.jsx
@@ -3,31 +3,56 @@ import React from 'react';
 export default class SearchForm extends React.Component {
   render() {
     return (
-      <form className="search-form">
-        <div className="location-container">
-          <input
-            required
-            id="location"
-            type="text"
-            name="location"
-            className="location-search"
-            placeholder="Location" />
-        </div>
-        <div className="food-type-container">
-          <button className="food-type" id="korean">Korean</button>
-          <button className="food-type" id="italian">Italian</button>
-          <button className="food-type" id="chinese">Chinese</button>
-          <button className="food-type" id="burgers">Burgers</button>
-          <button className="food-type" id="japanese">Japanese</button>
-          <button className="food-type" id="thai">Thai</button>
-          <button className="food-type" id="mexican">Mexican</button>
-          <button className="food-type" id="pizza">Pizza</button>
-          <button className="food-type" id="vietnamese">Vietnamese</button>
-        </div>
-        <div className="search-button-container">
-          <button className="search-button" type="submit">Search</button>
-        </div>
-      </form>
+      <>
+        <form className="search-form mobile">
+          <div className="location-container">
+            <input
+              required
+              id="location"
+              type="text"
+              name="location"
+              className="location-search"
+              placeholder="Location" />
+          </div>
+          <div className="food-type-container">
+            <button className="food-type" id="korean">Korean</button>
+            <button className="food-type" id="italian">Italian</button>
+            <button className="food-type" id="chinese">Chinese</button>
+            <button className="food-type" id="burgers">Burgers</button>
+            <button className="food-type" id="japanese">Japanese</button>
+            <button className="food-type" id="thai">Thai</button>
+            <button className="food-type" id="mexican">Mexican</button>
+            <button className="food-type" id="pizza">Pizza</button>
+            <button className="food-type" id="vietnamese">Vietnamese</button>
+          </div>
+          <div className="search-button-container">
+            <button className="search-button" type="submit">Search</button>
+          </div>
+        </form>
+        <form className="search-form desktop">
+          <div className="location-container">
+            <input
+              required
+              id="location"
+              type="text"
+              name="location"
+              className="location-search"
+              placeholder="Location" />
+            <button className="search-button" type="submit">Search</button>
+          </div>
+          <div className="food-type-container">
+            <button className="food-type" id="korean">Korean</button>
+            <button className="food-type" id="italian">Italian</button>
+            <button className="food-type" id="chinese">Chinese</button>
+            <button className="food-type" id="burgers">Burgers</button>
+            <button className="food-type" id="japanese">Japanese</button>
+            <button className="food-type" id="thai">Thai</button>
+            <button className="food-type" id="mexican">Mexican</button>
+            <button className="food-type" id="pizza">Pizza</button>
+            <button className="food-type" id="vietnamese">Vietnamese</button>
+          </div>
+        </form>
+      </>
     );
   }
 }
