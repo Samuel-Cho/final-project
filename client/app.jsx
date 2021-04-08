eb1fb112c9c46cbe05352ccc73c4b0f3c1099d58
SamuelCho1 <samuel.ms.cho@gmail.com>
2021-04-08 18:54:23 +0000

    Exported parseRoute to index.js and exported parseRoute from index.js. #2

--- client/app.jsx
+++ client/app.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Home from './pages/home';
 import Header from './components/header';
+// import {parseRoute} from './lib';
 
 export default class App extends React.Component {
   render() {
