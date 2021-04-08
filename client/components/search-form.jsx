dc15ea092958d0b752f3cd68c04d3dcb6d524d67
SamuelCho1 <samuel.ms.cho@gmail.com>
2021-04-08 01:20:33 +0000

    Edit render method for SearchForm to map through foodType array to create React elements for mobile view. #2

--- client/components/search-form.jsx
+++ client/components/search-form.jsx
@@ -1,5 +1,44 @@
 import React from 'react';
 
+const foodTypeList = [
+  {
+    id: 'korean',
+    text: 'Korean'
+  },
+  {
+    id: 'italian',
+    text: 'Italian'
+  },
+  {
+    id: 'chinese',
+    text: 'Chinese'
+  },
+  {
+    id: 'burgers',
+    text: 'Burgers'
+  },
+  {
+    id: 'japanese',
+    text: 'Japanese'
+  },
+  {
+    id: 'thai',
+    text: 'Thai'
+  },
+  {
+    id: 'mexican',
+    text: 'Mexican'
+  },
+  {
+    id: 'pizza',
+    text: 'Pizza'
+  },
+  {
+    id: 'vietnamese',
+    text: 'Vietnamese'
+  }
+];
+
 export default class SearchForm extends React.Component {
   constructor(props) {
     super(props);
@@ -23,6 +62,26 @@ export default class SearchForm extends React.Component {
   }
 
   render() {
+    let divItems = null;
+    if (this.state.foodType === '') {
+      divItems = foodTypeList.map(foodType => {
+        return (
+          <div onClick={this.handleClick} className="food-type" id={foodType.id} key={foodType.id}>{foodType.text}</div>
+        );
+      });
+    } else {
+      divItems = foodTypeList.map(foodType => {
+        if (foodType.id === this.state.foodType) {
+          return (
+            <div onClick={this.handleClick} className="food-type selected" id={foodType.id} key={foodType.id}>{foodType.text}</div>
+          );
+        } else {
+          return (
+            <div onClick={this.handleClick} className="food-type" id={foodType.id} key={foodType.id}>{foodType.text}</div>
+          );
+        }
+      });
+    }
     return (
       <>
         <form className="search-form mobile">
@@ -38,7 +97,8 @@ export default class SearchForm extends React.Component {
               value={this.state.value} />
           </div>
           <div className="food-type-container">
-            <div onClick={this.handleClick} className="food-type" id="korean">Korean</div>
+            {divItems}
+            {/* <div onClick={this.handleClick} className="food-type" id="korean">Korean</div>
             <div onClick={this.handleClick} className="food-type" id="italian">Italian</div>
             <div onClick={this.handleClick} className="food-type" id="chinese">Chinese</div>
             <div onClick={this.handleClick} className="food-type" id="burgers">Burgers</div>
@@ -46,7 +106,7 @@ export default class SearchForm extends React.Component {
             <div onClick={this.handleClick} className="food-type" id="thai">Thai</div>
             <div onClick={this.handleClick} className="food-type" id="mexican">Mexican</div>
             <div onClick={this.handleClick} className="food-type" id="pizza">Pizza</div>
-            <div onClick={this.handleClick} className="food-type" id="vietnamese">Vietnamese</div>
+            <div onClick={this.handleClick} className="food-type" id="vietnamese">Vietnamese</div> */}
           </div>
           <div className="search-button-container">
             <button className="search-button" type="submit">Search</button>
