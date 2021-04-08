0d21fffe60ceb83e133eaa6af6a9389f53eb34b3
SamuelCho1 <samuel.ms.cho@gmail.com>
2021-04-08 22:24:54 +0000

    Created error-middleware.js and set errorMiddleware function as default export. #4

---
+++ server/error-middleware.js
@@ -0,0 +1,14 @@
+const ClientError = require('./client-error');
+
+export default function errorMiddleware(err, req, res, next) {
+  if (err instanceof ClientError) {
+    res.status(err.status).json({
+      error: err.message
+    });
+  } else {
+    console.error(err);
+    res.status(500).json({
+      error: 'an unexpected error has occured'
+    });
+  }
+}
