6a455f0c89d6b75d00a0d225ff91f733aeaf451a
SamuelCho1 <samuel.ms.cho@gmail.com>
2021-04-08 19:03:43 +0000

    Created constructor method in App component and created state object with route property with value using parseRoute function. #2

--- client/app.jsx
+++ client/app.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import Home from './pages/home';
 import Header from './components/header';
-// import {parseRoute} from './lib';
+import { parseRoute } from './lib';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      route: parseRoute(window.location.hash)
+    };
+  }
+
   render() {
+    // console.log(this.state);
     return (
     <>
       <Header />
