05ab0c27636fcc14ba1e937d6ed55a70a9d38889
SamuelCho1 <samuel.ms.cho@gmail.com>
2021-04-09 02:52:37 +0000

    Added results of API call to this.state.restaurants. #4

--- client/pages/search-results.jsx
+++ client/pages/search-results.jsx
@@ -5,14 +5,15 @@ export default class SearchResults extends React.Component {
     super(props);
     this.state = {
       location: props.location,
-      foodType: props.foodType
+      foodType: props.foodType,
+      restaurants: null
     };
   }
 
   componentDidMount() {
     fetch(`/api/search/${this.state.location}/${this.state.foodType}`)
-      .then(res => res.json());
-    // .then(businesses => console.log(businesses));
+      .then(res => res.json())
+      .then(businesses => this.setState({ restaurants: businesses.businesses }));
   }
 
   render() {
