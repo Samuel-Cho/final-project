74aaa47879e048c3ad94f9e92119adb98758643d
SamuelCho1 <samuel.ms.cho@gmail.com>
2021-04-12 22:31:35 +0000

    Edit render method in App component and created AppContext.Provider react element with handleClick and drawerClosed. #7

--- client/app.jsx
+++ client/app.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Home from './pages/home';
 import Header from './components/header';
-import { parseRoute } from './lib';
+import { parseRoute, AppContext } from './lib';
 import SearchResults from './pages/search-results';
 import PageWrapper from './components/page-wrapper';
 
@@ -43,13 +43,18 @@ export default class App extends React.Component {
   }
 
   render() {
+    const { drawerClosed } = this.state;
+    const { handleClick } = this;
+    const contextValue = { drawerClosed, handleClick };
     return (
-    <>
-      <PageWrapper path={this.state.route.path}>
-        <Header />
-        {this.renderPage()}
-      </PageWrapper>
-    </>
+      <AppContext.Provider value={contextValue}>
+        <>
+          <PageWrapper path={this.state.route.path}>
+            <Header />
+            {this.renderPage()}
+          </PageWrapper>
+        </>
+      </AppContext.Provider>
     );
   }
 }
