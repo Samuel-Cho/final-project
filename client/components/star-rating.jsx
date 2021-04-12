0f9323b714d4b7bf6859bc599140539ea3e28905
SamuelCho1 <samuel.ms.cho@gmail.com>
2021-04-12 17:56:16 +0000

    Applied feedback to simplify code to create star icon elements in StarRating component. #5

--- client/components/star-rating.jsx
+++ client/components/star-rating.jsx
@@ -2,115 +2,14 @@ import React from 'react';
 
 export default function StarRating(props) {
   const rating = props.rating;
-  if (rating === 5) {
-    return (
-      <>
-        <i className="fas fa-star star"></i>
-        <i className="fas fa-star star"></i>
-        <i className="fas fa-star star"></i>
-        <i className="fas fa-star star"></i>
-        <i className="fas fa-star star"></i>
-      </>
-    );
-  } else if (rating === 4.5) {
-    return (
-      <>
-        <i className="fas fa-star star"></i>
-        <i className="fas fa-star star"></i>
-        <i className="fas fa-star star"></i>
-        <i className="fas fa-star star"></i>
-        <i className="fas fa-star-half-alt star"></i>
-      </>
-    );
-  } else if (rating === 4) {
-    return (
-      <>
-        <i className="fas fa-star star"></i>
-        <i className="fas fa-star star"></i>
-        <i className="fas fa-star star"></i>
-        <i className="fas fa-star star"></i>
-        <i className="far fa-star star"></i>
-      </>
-    );
-  } else if (rating === 3.5) {
-    return (
-      <>
-        <i className="fas fa-star star"></i>
-        <i className="fas fa-star star"></i>
-        <i className="fas fa-star star"></i>
-        <i className="fas fa-star-half-alt star"></i>
-        <i className="far fa-star star"></i>
-      </>
-    );
-  } else if (rating === 3) {
-    return (
-      <>
-        <i className="fas fa-star star"></i>
-        <i className="fas fa-star star"></i>
-        <i className="fas fa-star star"></i>
-        <i className="far fa-star star"></i>
-        <i className="far fa-star star"></i>
-      </>
-    );
-  } else if (rating === 2.5) {
-    return (
-      <>
-        <i className="fas fa-star star"></i>
-        <i className="fas fa-star star"></i>
-        <i className="fas fa-star-half-alt star"></i>
-        <i className="far fa-star star"></i>
-        <i className="far fa-star star"></i>
-      </>
-    );
-  } else if (rating === 2) {
-    return (
-      <>
-        <i className="fas fa-star star"></i>
-        <i className="fas fa-star star"></i>
-        <i className="far fa-star star"></i>
-        <i className="far fa-star star"></i>
-        <i className="far fa-star star"></i>
-      </>
-    );
-  } else if (rating === 1.5) {
-    return (
-      <>
-        <i className="fas fa-star star"></i>
-        <i className="fas fa-star-half-alt star"></i>
-        <i className="far fa-star star"></i>
-        <i className="far fa-star star"></i>
-        <i className="far fa-star star"></i>
-      </>
-    );
-  } else if (rating === 1) {
-    return (
-      <>
-        <i className="fas fa-star star"></i>
-        <i className="far fa-star star"></i>
-        <i className="far fa-star star"></i>
-        <i className="far fa-star star"></i>
-        <i className="far fa-star star"></i>
-      </>
-    );
-  } else if (rating === 0.5) {
-    return (
-      <>
-        <i className="fas fa-star-half-alt star"></i>
-        <i className="far fa-star star"></i>
-        <i className="far fa-star star"></i>
-        <i className="far fa-star star"></i>
-        <i className="far fa-star star"></i>
-      </>
-    );
-  } else {
-    return (
-      <>
-        <i className="far fa-star star"></i>
-        <i className="far fa-star star"></i>
-        <i className="far fa-star star"></i>
-        <i className="far fa-star star"></i>
-        <i className="far fa-star star"></i>
-      </>
-    );
-  }
+  const starElements = Array(5).fill(null).map((_, index) => {
+    if (index + 1 <= rating) {
+      return <i key={index} className="fas fa-star star" />;
+    }
+    if (index + 0.5 === rating) {
+      return <i key={index} className="fas fa-star-half-alt star" />;
+    }
+    return <i key={index} className="far fa-star star" />;
+  });
+  return starElements;
 }
