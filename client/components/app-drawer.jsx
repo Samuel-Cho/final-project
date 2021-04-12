33abb90ae3e29cf9fbde941450526e13c54f5092
SamuelCho1 <samuel.ms.cho@gmail.com>
2021-04-12 23:07:02 +0000

    Edit render method for AppDrawer to create react element for hidden app drawer. #7

--- client/components/app-drawer.jsx
+++ client/components/app-drawer.jsx
@@ -4,9 +4,27 @@ import AppContext from '../lib';
 export default class AppDrawer extends React.Component {
   render() {
     const { drawerClosed, handleClick } = this.context;
-    return (
-      <div onClick={handleClick}>{`placeholder ${drawerClosed}`}</div>
-    );
+    if (drawerClosed === true) {
+      return (
+        <div className="app-drawer-container hidden">
+          <div clasName="menu-container">
+            <h2>Menu</h2>
+            <ul>
+              <li onClick={handleClick}>
+                <i className="fas fa-search search-icon">  Search Restaruant</i>
+              </li>
+              <li onClick={handleClick}>
+                <i className="fas fa-list list-icon">  Restaurant List</i>
+              </li>
+              <li onClick={handleClick}>
+                <i className="fas fa-check-circle check=circle-icon">  Randomize List</i>
+              </li>
+            </ul>
+          </div>
+          <div className="modal" onClick={handleClick}></div>
+        </div>
+      );
+    }
   }
 }
 
