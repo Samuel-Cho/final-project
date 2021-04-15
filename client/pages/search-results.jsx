746f02691e9250307cf04e21b5d62bf63772779d
SamuelCho1 <samuel.ms.cho@gmail.com>
2021-04-15 20:03:50 +0000

    Edit SearchResults to return only one react element to fix fix duplicative icon renders. #9

--- client/pages/search-results.jsx
+++ client/pages/search-results.jsx
@@ -24,30 +24,7 @@ export default class SearchResults extends React.Component {
 
   render() {
     const restaurants = this.state.restaurants;
-    const divRestaruantMobile = restaurants.map(restaurant => {
-      return (
-        <div key={restaurant.alias} className="restaurant one-third-column">
-          <a className="restaurant-link" href={restaurant.url} target="_blank" rel="noreferrer">
-            <div className="image-container">
-              <img className="restaurant-image" src={restaurant.image_url}></img>
-            </div>
-            <div className="detail-container">
-              <p className="restaurant-name">{restaurant.name}</p>
-              <p className="restaurant-address">{restaurant.location.address1}</p>
-              <div className="restaurant-rating">
-                <StarRating rating={restaurant.rating} />
-              </div>
-              <p className="restaurant-review-count">{`Based on ${restaurant.review_count} Review`}</p>
-            </div>
-          </a>
-          <div className="select-icon-container">
-            <CheckIcon restaurant={restaurant} restaurantsDbAliases={this.state.restaurantsDbAliases} />
-          </div>
-        </div>
-
-      );
-    });
-    const divRestaruantDesktop = restaurants.map(restaurant => {
+    const divRestaruant = restaurants.map(restaurant => {
       return (
         <div key={restaurant.alias} className="restaurant one-third-column">
           <div className="image-container">
@@ -74,14 +51,9 @@ export default class SearchResults extends React.Component {
       );
     });
     return (
-      <>
-        <div className="restaurant-list-container mobile">
-          {divRestaruantMobile}
-        </div>
-        <div className="restaurant-list-container desktop">
-          {divRestaruantDesktop}
-        </div>
-      </>
+      <div className="restaurant-list-container ">
+        {divRestaruant}
+      </div>
     );
   }
 }
