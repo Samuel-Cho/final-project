1a0ffd948a41d71c689e73cdb49449cb5ee5e687
SamuelCho1 <samuel.ms.cho@gmail.com>
2021-04-16 17:51:02 +0000

    Created randomize-list.jsx in pages directory with appropriate imports. #11

---
+++ client/pages/randomize-list.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+// import StarRating from '../components/star-rating';
+// import CheckIcon from '../components/check-icon';
+
+export default class RandomizeList extends React.Component {
+  render() {
+    return null;
+  }
+}
