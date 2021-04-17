33c22a4ee62b1131548381e8ecddd3be5db65edb
SamuelCho1 <samuel.ms.cho@gmail.com>
2021-04-17 00:55:30 +0000

    Created constructor method and state object. #12

--- client/pages/eat-here.jsx
+++ client/pages/eat-here.jsx
@@ -2,6 +2,13 @@ import React from 'react';
 // import StarRating from '../components/star-rating';
 
 export default class EatHere extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      restaurants: []
+    };
+  }
+
   render() {
     return null;
   }
