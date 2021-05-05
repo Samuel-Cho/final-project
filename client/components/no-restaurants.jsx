b2d22b3e7790e70c8d294a2ed6329180b6873020
SamuelCho1 <samuel.ms.cho@gmail.com>
2021-05-05 23:24:33 +0000

    Created message to user when error with API call has occured for RandomizeList. #26 #38

--- client/components/no-restaurants.jsx
+++ client/components/no-restaurants.jsx
@@ -2,7 +2,35 @@ import React from 'react';
 import { AppContext } from '../lib';
 
 export default class NoRestaurant extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: false
+    };
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.error !== this.props.error && prevState.error !== this.props.error) {
+      this.setState({ error: this.props.error });
+    }
+  }
+
   render() {
+    if (this.state.error === true) {
+      if (this.context.route.path === 'searchResults') {
+        return (
+          <div className="no-restaurant-container">
+            <p className="no-restaurant">Error with API has occured</p>
+          </div>
+        );
+      } else {
+        return (
+          <div className="no-restaurant-container">
+            <p className="no-restaurant white">Error with API has occured</p>
+          </div>
+        );
+      }
+    }
     if (this.context.route.path === 'searchResults') {
       return (
         <div className="no-restaurant-container">
--- client/pages/randomize-list.jsx
+++ client/pages/randomize-list.jsx
@@ -16,59 +16,87 @@ export default class RandomizeList extends React.Component {
 
   componentDidMount() {
     fetch('/api/randomizerListAll')
-      .then(res => res.json())
+      .then(res => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          return false;
+        }
+      })
       .then(results => {
-        const restaurantsConverted = results.map(restaurant => {
-          const restaurantConverted = {
-            id: restaurant.id,
-            alias: restaurant.alias,
-            url: restaurant.url,
-            image_url: restaurant.imageUrl,
-            name: restaurant.name,
-            location: {
-              address1: restaurant.address1
-            },
-            rating: restaurant.rating,
-            review_count: restaurant.reviewCount
-          };
-          return restaurantConverted;
-        });
-        const restaurantAliases = results.map(restaurant => {
-          return { alias: restaurant.alias };
-        });
-        this.setState({ restaurantsDbAliases: restaurantAliases, restaurants: restaurantsConverted, loading: false });
+        if (results) {
+          const restaurantsConverted = results.map(restaurant => {
+            const restaurantConverted = {
+              id: restaurant.id,
+              alias: restaurant.alias,
+              url: restaurant.url,
+              image_url: restaurant.imageUrl,
+              name: restaurant.name,
+              location: {
+                address1: restaurant.address1
+              },
+              rating: restaurant.rating,
+              review_count: restaurant.reviewCount
+            };
+            return restaurantConverted;
+          });
+          const restaurantAliases = results.map(restaurant => {
+            return { alias: restaurant.alias };
+          });
+          this.setState({ restaurantsDbAliases: restaurantAliases, restaurants: restaurantsConverted, loading: false });
+        } else {
+          this.setState({ loading: false, restaurants: ['error'] });
+        }
       });
   }
 
   render() {
     const restaurants = this.state.restaurants;
-    const divRestaruants = restaurants.map(restaurant => {
+    if (restaurants[0] === 'error') {
       return (
-        <li key={restaurant.alias} className="restaurant-randomize one-third-column" id={restaurant.id}>
-          <div className="image-container">
-            <a href={restaurant.url} target="_blank" rel="noreferrer">
-              <img className="restaurant-image" src={restaurant.image_url}></img>
-            </a>
-          </div>
-          <div className="bottom-container">
-            <a className="restaurant-link" href={restaurant.url} target="_blank" rel="noreferrer">
-              <div className="detail-container">
-                <p className="restaurant-name">{restaurant.name}</p>
-                <p className="restaurant-address">{restaurant.location.address1}</p>
-                <div className="restaurant-rating">
-                  <StarRating rating={restaurant.rating} />
-                </div>
-                <p className="restaurant-review-count">{`Based on ${restaurant.review_count} Review`}</p>
-              </div>
-            </a>
-            <div className="select-icon-container">
-              <CheckIcon restaurant={restaurant} restaurantsDbAliases={this.state.restaurantsDbAliases} />
+        <>
+          <Loading loading={this.state.loading} />
+          <div className="container mobile">
+            <div className="randomize-list-container">
+              <h2 className="randomize-list-header">Randomize List</h2>
+              <NoRestaurants error={true}/>
             </div>
           </div>
-        </li>
+          <div className="container desktop">
+            <div className="randomize-list-container">
+              <NoRestaurants error={true}/>
+            </div>
+          </div>
+        </>
       );
-    });
+    }
     if (restaurants.length !== 0) {
+      const divRestaruants = restaurants.map(restaurant => {
+        return (
+          <li key={restaurant.alias} className="restaurant-randomize one-third-column" id={restaurant.id}>
+            <div className="image-container">
+              <a href={restaurant.url} target="_blank" rel="noreferrer">
+                <img className="restaurant-image" src={restaurant.image_url}></img>
+              </a>
+            </div>
+            <div className="bottom-container">
+              <a className="restaurant-link" href={restaurant.url} target="_blank" rel="noreferrer">
+                <div className="detail-container">
+                  <p className="restaurant-name">{restaurant.name}</p>
+                  <p className="restaurant-address">{restaurant.location.address1}</p>
+                  <div className="restaurant-rating">
+                    <StarRating rating={restaurant.rating} />
+                  </div>
+                  <p className="restaurant-review-count">{`Based on ${restaurant.review_count} Review`}</p>
+                </div>
+              </a>
+              <div className="select-icon-container">
+                <CheckIcon restaurant={restaurant} restaurantsDbAliases={this.state.restaurantsDbAliases} />
+              </div>
+            </div>
+          </li>
+        );
+      });
       return (
         <>
           <Loading loading={this.state.loading} />
