e97e26e0eefe8b91d8ac9b6b951b0354097cb89e
SamuelCho1 <samuel.ms.cho@gmail.com>
2021-05-07 19:32:08 +0000

    Added alt property for restaurant images. #26

--- client/pages/search-results.jsx
+++ client/pages/search-results.jsx
@@ -77,7 +77,7 @@ export default class SearchResults extends React.Component {
           <li key={restaurant.alias} className="restaurant one-third-column" id={restaurant.id}>
             <div className="image-container">
               <a href={restaurant.url} target="_blank" rel="noreferrer">
-                <img className="restaurant-image" src={restaurant.image_url}></img>
+                <img className="restaurant-image" alt="restaurant image" src={restaurant.image_url}></img>
               </a>
             </div>
             <div className="bottom-container">
